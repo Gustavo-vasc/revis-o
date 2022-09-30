cfcba55b07a19b1bdfc6c6ca5a3a602865a33693
Gustavo-Vasc <emivi@example.com>
2022-09-29 22:43:01 -0300

    revisão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { CombosComponent } from './components/produtos/combos/combos.component';
+import { BuildsComponent } from './components/produtos/builds/builds.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-
+  {path:'produtos', component:BuildsComponent},
+  {path:'produtos/combos', component:CombosComponent}
 ];
 
 @NgModule({
